aba1e7a6d6319d49bd1a85c91b3b4e8cce9e2fb9
Blake Sanie <blake@sanie.com>
2018-12-29 23:00:00 -0800

    categories added

--- functions.js
+++ functions.js
@@ -2,11 +2,9 @@ $("h2:nth-of-type(1)").click(function() {
   showCategories();
 });
 
-$("#cover")
-  .not("h2:nth-of-type(1)")
-  .click(function() {
-    hideCategories();
-  });
+$("#cover, #cancel").click(function() {
+  hideCategories();
+});
 
 function showCategories() {
   $("#categories").css({
@@ -27,3 +25,46 @@ function hideCategories() {
     });
   }, 200);
 }
+
+$(document).scroll(function() {
+  var y = $(document).scrollTop();
+  //$("h3").css("margin-top", -1 * y);
+  /*if (y > 60) {
+    $("#cover").css("transform", "translateY(" + (y - 60) / 2 + "px)");
+    $("h3").css("margin-top", -1 * (y - 60));
+}*/
+});
+
+$(".shade").click(function() {
+  $(this).css({ opacity: 0, "pointer-events": "none" });
+});
+
+$(".cat").click(function() {
+  var opacity = $(this)
+    .find(".shade")
+    .css("opacity");
+  if (opacity == 0) {
+    $(this)
+      .find(".shade")
+      .css({ opacity: 1, "pointer-events": "auto" });
+  }
+});
+
+$(".cat").hover(
+  function() {
+    $(this)
+      .find(".shade")
+      .css({
+        opacity: 1,
+        "pointer-events": "auto"
+      });
+  },
+  function() {
+    $(this)
+      .find(".shade")
+      .css({
+        opacity: 0,
+        "pointer-events": "none"
+      });
+  }
+);
