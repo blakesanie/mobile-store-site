1e82f3cd15e79628f4b088852f0861fcf8c6abdc
Blake Sanie <blake@sanie.com>
2019-01-06 22:49:46 -0800

    all working? lets check on mobile

--- thumbFunctions.js
+++ thumbFunctions.js
@@ -1,50 +1,49 @@
-$(document).on("mousedown", ".clickOff", function() {
-  //clickedOff($(this), e);
+$(document).on("mousedown", ".clickOff", function(e) {
   fadeOut($(this));
 });
 
-$(document).on("mousedown", ".item", function() {
+$(document).on("mousedown", ".item", function(e) {
   fadeIn($(this));
 });
 
-$(document).on("touchstart", ".clickOff", function() {
-  //clickedOff($(this), e);
+$(document).on("touchstart", ".clickOff", function(e) {
   fadeOut($(this));
 });
 
-$(document).on("touchstart", ".item", function() {
+$(document).on("touchstart", ".item", function(e) {
   fadeIn($(this));
 });
 
-/*$(document).on("mousedown", "#clickAllOff", function() {
+$(document).on("mousedown", "#clickAllOff", function() {
   fadeOutAllExcept(-1);
 });
-
 $(document).on("touchstart", "#clickAllOff", function() {
   fadeOutAllExcept(-1);
-});*/
-
-//touchstart
-
-//$(".item").hover(function() {}, function() {});
-/*
-$(document).on("mouseleave", ".shade:not(.view)", function() {
-  fadeOut($(this));
 });
 
+$(document).on("mouseleave", ".clickOff", function() {
+  fadeOut($(this));
+});
 $(document).on("mouseenter", ".item", function() {
   fadeIn($(this));
 });
-*/
 
-function fadeOut(element /*, e*/) {
+function fadeOut(element) {
   element.parent().css({ opacity: 0, "pointer-events": "none" });
 }
 
 function fadeIn(element) {
-  var index = element.index;
+  var index = element.index();
   fadeOutAllExcept(index);
-  element.find(".shade").css({ opacity: 1, "pointer-events": "auto" });
+  $(".item").each(function(i) {
+    if (i != index) {
+      fadeOut($(this).find(".clickOff"));
+    }
+  });
+  var opacity = element.find(".shade").css("opacity");
+  if (opacity == 0) {
+    element.find(".shade").css({ opacity: 1, "pointer-events": "auto" });
+  }
 }
 
 function fadeOutAllExcept(index) {
@@ -54,24 +53,3 @@ function fadeOutAllExcept(index) {
     }
   });
 }
-/*
-function clickedOff(element, e) {
-  console.log(viewClicked(element, e));
-  if (!viewClicked(element, e)) {
-    fadeOut(element);
-  }
-}
-
-function viewClicked(element, e) {
-  var view = element.find(".view");
-  var x = e.pageX;
-  var y = e.pageY;
-  var rect = view[0].getBoundingClientRect();
-  console.log(rect);
-  var X1 = rect.left;
-  var Y1 = rect.top;
-  var X2 = rect.right;
-  var Y2 = rect.bottom;
-  console.log(X1, x, X2, Y1, y, Y2);
-  return x >= X1 && x <= X2 && y >= Y1 && y <= Y2;
-}*/
