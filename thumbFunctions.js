2d331db01557989669dce9b8b5023d35e781aae8
Blake Sanie <blake@sanie.com>
2019-01-08 08:40:16 -0800

    css hover

--- thumbFunctions.js
+++ thumbFunctions.js
@@ -4,17 +4,17 @@ $(document).on("mousedown", ".clickOff", function(e) {
   fadeOut($(this));
 });
 
-$(document).on("mousedown", ".item", function(e) {
+/*$(document).on("mousedown", ".item", function(e) {
   fadeIn($(this));
-});
+});#*/
 
 $(document).on("touchend", ".clickOff", function(e) {
   fadeOut($(this));
 });
 
-$(document).on("touchend", ".item", function(e) {
+/*$(document).on("touchend", ".item", function(e) {
   fadeIn($(this));
-});
+});*/
 
 $(document).on("mousedown", "#clickAllOff", function() {
   fadeOutAllExcept(-1);
@@ -23,12 +23,12 @@ $(document).on("touchend", "#clickAllOff", function() {
   fadeOutAllExcept(-1);
 });
 
-$(document).on("mouseleave", ".shade", function() {
+/*$(document).on("mouseleave", ".shade", function() {
   fadeOut($(this).find(".clickOff"));
 });
 $(document).on("mouseenter", ".item", function() {
   fadeIn($(this));
-});
+});*/
 
 function fadeOut(element) {
   console.log("fadeout");
@@ -42,7 +42,7 @@ function fadeIn(element) {
   console.log("fadein");
   var index = element.index();
   fadeOutAllExcept(index);
-  element.find(".shade").css({ opacity: 0.5 });
+  element.find(".shade").css({ opacity: 0.1 });
   setTimeout(function() {
     element.find(".shade").css({ "pointer-events": "auto" });
   }, 50);
