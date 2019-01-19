03d4c7ecea61892cdc87bdd539bae801b4ab032c
Blake Sanie <blake@sanie.com>
2019-01-19 09:54:04 -0800

    logo and stuff added

--- base.js
+++ base.js
@@ -5,24 +5,34 @@ $("#nav a:nth-of-type(2)").click(function() {
 $("body > *:not(#header)").click(function() {
   console.log("hide");
   hideCategories();
-});
+}); // does this even work?
 
 $("#categories #cancel").click(function() {
   hideCategories();
 });
 
+$(document).click(function(e) {
+  if (e.clientY > 120) {
+    hideCategories();
+  }
+});
+
 function showCategories() {
   $("#categories").css({
-    display: "flex"
-  });
-  $("#categories").css({
+    display: "flex",
     opacity: 1
+  });
+  $("#header").css({
+    "background-color": "rgba(255,255,255,0.7)"
   });
 }
 
 function hideCategories() {
   $("#categories").css({
     opacity: 0
+  });
+  $("#header").css({
+    "background-color": "transparent"
   });
   setTimeout(function() {
     $("#categories").css({
