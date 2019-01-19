370db00e5c25c6ef27b8e514c6a739ebbd777b4b
Blake Sanie <blake@sanie.com>
2019-01-18 17:53:42 -0800

    search removed from nav, paralax disabled, other small style fixes

--- functions.js
+++ functions.js
@@ -1,21 +1,24 @@
 $(document).scroll(function() {
-  var y = $(document).scrollTop();
-  var height = $(window).height();
-  if (y > 60) {
-    $("#cover").css({
-      height: Math.max(height - y, 0) + "px",
-      "margin-top": Math.min(y - 60, height) + "px"
-    });
-    $("h3").css({
-      opacity: 1 - (y / height) * 1.2
-    });
-  } else {
-    $("#cover").css({
-      height: height - 60 + "px",
-      "margin-top": 0
-    });
-    $("h3").css({
-      opacity: 1
-    });
-  }
+  // var y = $(document).scrollTop();
+  // var height = $(window).height();
+  // var width = $(window).width();
+  // var headerHeight = width > 500 ? 60 : 110;
+  //$("h3").animate({ scrollTop: y * 100 });
+  // if (y > headerHeight) {
+  //   $("#cover").css({
+  //     height: Math.max(height - y, 0) + "px",
+  //     "margin-top": Math.min(y - headerHeight, height) + "px"
+  //   });
+  //   $("h3").css({
+  //     opacity: 1 - (y / height) * 1.2
+  //   });
+  // } else {
+  //   $("#cover").css({
+  //     height: height - headerHeight + "px",
+  //     "margin-top": 0
+  //   });
+  //   $("h3").css({
+  //     opacity: 1
+  //   });
+  // }
 });
